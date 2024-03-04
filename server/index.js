35f5ac39ca261105b3a6a4f0090b5105098fc6dd
DH0710 <DamienHebert710@gmail.com>
2024-03-03 19:14:49 -0600

    finally connected database to mongoDB adding dotenv to index.js and also installing dotenv to process file

--- server/index.js
+++ server/index.js
@@ -2,15 +2,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const connectDB = require("./config/dbConnection")
+const dotenv = require("dotenv").config();
 
 connectDB();
+
+
+app.use(express.json());
+app.use('/api/sneakers', require ("./routes/sneakerRoutes"));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-app.use(express.json());
-
-
-
-app.use('/api/sneakers', require ("./routes/sneakerRoutes"));
