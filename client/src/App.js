302bae3d67eb06b02039b432336929bd4f24e5c2
DH0710 <DamienHebert710@gmail.com>
2024-06-09 21:49:53 -0500

    changed switch to route on app.js and now the page rendered like its suppose to, navbar and links show up for home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
 
 // Screen 
 import LoginScreen from "./screens/LoginScreen"
@@ -8,22 +8,25 @@ import HomeScreen from "./screens/HomeScreen"
 import SneakerScreen from "./screens/SneakerScreen"
 import CartScreen from "./screens/CartScreen"
 
+// Components
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <Router>
     
       
-     { /* Navbar */}
+     <Navbar />
      { /* Side Drawer   */}
      {/* Back drop  */}
 
      <main>
-      <Switch>
+      <Routes>
         <Route exact path= "/login" component= {LoginScreen} />
         <Route exact path= "/home" component= {HomeScreen} />
         <Route exact path= "/sneaker/:id" component= {SneakerScreen} />
         <Route exact path= "/cart" componennt= {CartScreen} />
-      </Switch>
+      </Routes>
      </main>
 
     
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <nav className='navbar'>
 
-        { /* logo */}
+       
         <div className='navbar_logo'> 
         <h2> Swish Kick </h2>
 
@@ -18,13 +18,31 @@ const Navbar = () => {
 
           <li>
             <Link to= "/cart">
+              <i className='fas fa-shopping-cart'> </i>
+            
+               Cart
+               <span className='cartlogo_badge'> 0 </span>
 
+            </Link>
+          </li>
+
+          <li>
+            <Link to= "/home">
+             Swish Kick Sneaker Store
             </Link>
           </li>
 
         </ul>
 
-        { /* hamburger menu */}
+        
+
+        <div className='hamburger_menu'>
+
+          <div></div>
+          <div></div>
+          <div></div>
+
+        </div>
       
     </nav>
   )
