5249ef02c47180a2be24c2108f6c9026188fe42d
DH0710 <DamienHebert710@gmail.com>
2024-06-11 22:03:36 -0500

    added styling to navbar and navbar links, as well as styling to cart link within navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,49 +1,51 @@
 import React from 'react'
 import "./Navbar.css"
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 
 const Navbar = () => {
   return (
     <nav className='navbar'>
 
-       
-        <div className='navbar_logo'> 
-        <h2> Swish Kick </h2>
 
-        </div>
+      <div className='navbar_logo'>
+        <h2> Swish Kicks </h2>
 
-        { /* links */}
+      </div>
 
-        <ul className='navbar_links'> 
-
-          <li>
-            <Link to= "/cart">
-              <i className='fas fa-shopping-cart'> </i>
-            
-               Cart
-               <span className='cartlogo_badge'> 0 </span>
-
-            </Link>
-          </li>
-
-          <li>
-            <Link to= "/home">
-             Swish Kick Sneaker Store
-            </Link>
-          </li>
-
-        </ul>
-
-        
-
-        <div className='hamburger_menu'>
-
-          <div></div>
-          <div></div>
-          <div></div>
-
-        </div>
       
+
+      <ul className='navbar_links'>
+
+        <li>
+          <Link to="/cart" className='cart_link'>
+            <i className='fas fa-shopping-cart'> </i>
+
+            <span>
+              Cart
+              <span className='cartlogo_badge'> 0 </span>
+            </span>
+
+          </Link>
+        </li>
+
+        <li>
+          <Link to="/">
+            Sneaker Store
+          </Link>
+        </li>
+
+      </ul>
+
+
+
+      <div className='hamburger_menu'>
+
+        <div></div>
+        <div></div>
+        <div></div>
+
+      </div>
+
     </nav>
   )
 }
