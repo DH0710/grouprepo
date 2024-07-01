8a81ffb3c4b02013a25ed5e90e0442ec3d6e48b9
DH0710 <DamienHebert710@gmail.com>
2024-06-30 20:12:10 -0500

    changed media queries, i think these are correct but if not i will change later

--- client/src/screens/SneakerScreen.js
+++ client/src/screens/SneakerScreen.js
@@ -5,10 +5,47 @@ const SneakerScreen = () => {
   return (
     <div className='sneakerscreen'>
 
-        <h2>Sneaker Screen</h2>
-      
+      <div classname="sneakerscreen-left">
+
+        <div className='left-image'>
+
+          <img src="https://m.media-amazon.com/images/I/71PkpSxlQ7L._AC_SX575_.jpg"
+            alt="sneaker-name" />
+        </div>
+
+        <div className='left-info'>
+          <p className='left-name'> Air Jordan Retro 3</p>
+          <p> Price: 199.99 </p>
+          <p> Colorway: White/Cement Gray  </p>
+
+        </div>
+
+
+
+
+      </div>
+
+
+
+
+
+
+      <div classname="sneakerscreen-right">
+        <div className='right-info'>
+          <p>
+            Price: <span> 299.99 </span>
+          </p>
+
+          <p>
+            Status: <span> In Stock </span>
+          </p>
+
+        </div>
+
+      </div>
+
     </div>
   )
 }
 
-export default SneakerScreen
+export default SneakerScreen;
